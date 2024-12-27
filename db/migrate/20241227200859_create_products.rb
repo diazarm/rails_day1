d13b705d358d770dde44edf4cf2566422b852c20
diazarm <marceloardiaz@gmail.com>
2024-12-27 19:02:49 -0300

    render products

--- db/migrate/20241227200859_create_products.rb
+++ db/migrate/20241227200859_create_products.rb
@@ -16,4 +16,15 @@ end
 
 #rails console  <- Para crear un nuevo producto por consola, lo primero es activar la consola.
 # Product.create(title: "Product 1", description: "Description 1", price:200)  <- Luego coloco la sig. sentencia para generar 1 prod. nuevo.
- 
+
+#con la consola activada (rails console)
+# Product.all  <- para ver todos los productos
+# Product.find(1)  <- para ver el producto con id 1
+# Product.find_by(title: "Product 1")  <- para ver el producto con el titulo
+# Product.where(title: "Product 1")  <- para ver todos los productos con el titulo
+# Product.where(title: "Product 1").first  <- para ver el primer producto con el
+# Product.where(title: "Product 1").last  <- para ver el ultimo producto con el
+# Product.where(title: "Product 1").count  <- para ver la cantidad de productos con
+# Product.where(title: "Product 1").destroy_all  <- para eliminar todos los productos con
+# Product.where(title: "Product 1").update_all(price: 300)  <- para
+# Product.where(title: "Product 1").update_all(price: 300, description: "
